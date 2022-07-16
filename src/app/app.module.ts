74bfd64f50b0932d46f1658a214800e6246c8cc4
lNotini <lucasnotini@hotmail.com>
2022-07-16 10:29:30 -0300

    Atualiza repositorio incluindo arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultasComponent } from './consultas/consultas.component';
+import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+
+//angular material
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConsultasComponent,
+    ApresentacaoComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+ 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
